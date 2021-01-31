214b9de88d96a0c8185ce92de41ea0d05eff9a35
GhostPlusX <david1000@gmail.com>
2021-02-01 01:22:36 +0900

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,3 +1,8 @@
+#Commit Git Hub Production
 git add . 
 git commit -m "Publish Update"
 git push -u origin main
+
+#Publish Pod Dependency
+#pod repo add GhostPlusX https://github.com/GhostPlusX/GhostPlusX.git
+#pod repo push GhostPlusX GhostPlusX.podspec
